cccd033822fd5fd48972889fd853b7da071dfbc5
Oliver <osales-2020584@kinal.edu.gt>
2024-05-22 01:57:43 -0600

    Se crearon los controladores y vistas de las 6 entidades restantes, se incluyeron imagenes para las entidades, solo dos vistas en scene builder se llegaron a completar

---
+++ src/org/oliversales/controller/MenuEmpleadosController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oliversales.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author olive
+ */
+public class MenuEmpleadosController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        // TODO
+    }    
+    
+}
