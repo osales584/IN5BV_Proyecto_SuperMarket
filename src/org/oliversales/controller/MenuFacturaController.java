90921afd64391328837ffdbf24bf309272a0c92e
Oliver <osales-2020584@kinal.edu.gt>
2024-06-13 23:59:24 -0600

    Se_modifico_la_clase_controller_de_factura,_agregando_metodos_y_la-accion_del_boton_de_reportes

--- src/org/oliversales/controller/MenuFacturaController.java
+++ src/org/oliversales/controller/MenuFacturaController.java
@@ -6,11 +6,21 @@
 package org.oliversales.controller;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.oliversales.bean.Factura;
+import org.oliversales.report.GenerarReportes;
 import org.oliversales.system.Principal;
 
 /**
@@ -20,13 +30,96 @@ import org.oliversales.system.Principal;
  */
 public class MenuFacturaController implements Initializable {
     private Principal escenarioPrincipal;
+    private enum operaciones {AGREGAR, ELIMINAR, EDITAR, ACTUALIZAR, CANCELAR, NINGUNO}
+    private operaciones tipoDeOperaciones = operaciones.NINGUNO;
+    
+    @FXML private TextField txtNumFact;
+    @FXML private TextField txtEstado;
+    @FXML private TextField txtTotalF;
+    @FXML private TextField txtFechaF;
+    @FXML private ComboBox cmbCodClien;
+    @FXML private ComboBox cmbCodEmp;
+    
+    @FXML private TableView tblFactura;
+    @FXML private TableColumn colNumFact;
+    @FXML private TableColumn colEstado;
+    @FXML private TableColumn colTotalF;
+    @FXML private TableColumn colFechaF;
+    @FXML private TableColumn colCodClien;
+    @FXML private TableColumn colCodEmp;
+
+    @FXML private Button btnAgregar;
+    @FXML private Button btnEliminar;
+    @FXML private Button btnEditar;
+    @FXML private Button btnReporte;
     @FXML private Button btnRegresar;
+    
+    //Imagenes
+    @FXML private ImageView imgAgregar;
+    @FXML private ImageView imgEliminar;
+    @FXML private ImageView imgEditar;
+    @FXML private ImageView imgReporte;
     
     
     @Override
     public void initialize(URL url, ResourceBundle resources) {
         // TODO
     }   
+    
+    public void reportes() {
+        switch (tipoDeOperaciones) {
+            case NINGUNO:
+                imprimirReporte();
+                break;
+            case ACTUALIZAR:
+                desactivarControles();
+                limpiarControles();
+                btnAgregar.setText("Agregar");
+                btnEliminar.setText("Eliminar");
+                btnEditar.setDisable(false);
+                btnReporte.setDisable(false);
+                imgAgregar.setImage(new Image("/org/oliversales/images/Agregar.png"));
+                imgEliminar.setImage(new Image("/org/oliversales/images/Eliminar.png"));         
+                tipoDeOperaciones = operaciones.NINGUNO;
+        }
+
+    }
+    
+    public void imprimirReporte(){
+        Map parametros = new HashMap();
+        int factId = ((Factura)tblFactura.getSelectionModel().getSelectedItem()).getNumeroFactura();
+        parametros.put("numeroFactura", factId);
+        GenerarReportes.mostrarReportes("ReporteFacturaVespertina.jasper", "Factura de la tarde", parametros);
+    }
+    
+// -----------------------------------------------------------------------------    
+    public void desactivarControles(){
+        txtNumFact.setEditable(false);
+        txtEstado.setEditable(false);
+        txtTotalF.setEditable(false);
+        txtFechaF.setEditable(false);
+        cmbCodClien.setEditable(true);
+        cmbCodEmp.setEditable(true);
+    }
+    
+    public void activarControles(){
+        txtNumFact.setEditable(true);
+        txtEstado.setEditable(true);
+        txtTotalF.setEditable(true);
+        txtFechaF.setEditable(true);
+        cmbCodClien.setEditable(false);
+        cmbCodEmp.setEditable(false);
+    }
+    
+    public void limpiarControles(){
+        txtNumFact.clear();
+        txtEstado.clear();
+        txtTotalF.clear();
+        txtFechaF.clear();
+        cmbCodClien.getSelectionModel().getSelectedItem();
+        cmbCodEmp.getSelectionModel().getSelectedItem();
+    } 
+    
     
 // -----------------------------------------------------------------------------    
     public Principal getEscenarioPrincipal() {
